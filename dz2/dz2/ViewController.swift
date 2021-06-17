acb98f62c7656090085f4d26a0c449e72bd13e26
Andrey <>
2021-06-17 19:14:13 +0300

    fix dz2

--- dz2/dz2/ViewController.swift
+++ dz2/dz2/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("1")
+        let a = 1
+        let b = 2
     }
 
 
