8c31cd77d154c4431906a8f72aa2a0b9022e1072
Andrey <>
2021-06-17 19:07:51 +0300

    dz2

---
+++ dz2/dz2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dz2
+//
+//  Created by Андрей Трофимов on 17.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
